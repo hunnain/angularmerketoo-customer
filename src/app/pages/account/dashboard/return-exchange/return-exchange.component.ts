f46bfe8b6bccb5401febc2b272b0b7b1304ae7a4
Abdul Samad <samadahmed112255@gmail.com>
2021-01-29 00:10:39 +0500

    dropzone added return and exchange

--- src/app/pages/account/dashboard/return-exchange/return-exchange.component.ts
+++ src/app/pages/account/dashboard/return-exchange/return-exchange.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-return-exchange',
-  templateUrl: './return-exchange.component.html',
-  styleUrls: ['./return-exchange.component.scss']
+  selector: "app-return-exchange",
+  templateUrl: "./return-exchange.component.html",
+  styleUrls: ["./return-exchange.component.scss"],
 })
 export class ReturnExchangeComponent implements OnInit {
+  constructor() {}
+  imgs = [];
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  readUrl(event: any) {
+    if (event.target.files.length === 0) return;
+    //Image upload validation
+    var mimeType = event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+    // Image upload
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (_event) => {
+      let img = reader.result.toString();
+      // let base = reader.result.toString();
+      console.log(img);
+      this.imgs.push(img);
+    };
   }
-
 }
